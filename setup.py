ff5edb3d03bc320eae28d7c38936c5fd2173bd77
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2022-09-07 15:06:18 -0700

    Fix packaging of the main.py module

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
         name='gene2pubmed-stats',
         version='1.0.0',
+        packages=['gene2pubmed_stats'],
         install_requires=['pandas'],
-        entry_points={'console_scripts': ['gene2pubmed-stats=main:main']})
+        entry_points={'console_scripts': ['gene2pubmed-stats=gene2pubmed_stats.main:main']})
