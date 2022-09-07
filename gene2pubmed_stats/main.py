407f196a6b89bd28af08fd8e4322e6312a97dd3b
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2022-09-07 15:16:00 -0700

    Fix subparser usage on Python 3.6

--- gene2pubmed_stats/main.py
+++ gene2pubmed_stats/main.py
@@ -9,16 +9,21 @@ logger = logging.getLogger(__name__)
 
 GENE2PUBMED_URL = 'https://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2pubmed.gz'
 
-def main():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(required=True)
-    parser_a = subparsers.add_parser('publications-per-gene')
-    parser_a.add_argument('--taxon-id', type=int)
-    args = parser.parse_args(sys.argv[1:])
+def publications_per_gene(args):
     logger.info('Reading from {}...'.format(GENE2PUBMED_URL))
     df = pd.read_csv(GENE2PUBMED_URL, sep='\t', compression='gzip')
     publication_counts_by_gene_id = df.groupby(['#tax_id', 'GeneID']).count().rename(columns={'PubMed_ID': 'Number of publications'})
     publication_counts_by_gene_id.loc[args.taxon_id].to_csv(sys.stdout, sep='\t')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.set_defaults(func=lambda args: parser.print_help() or 1)
+    subparsers = parser.add_subparsers()
+    parser_a = subparsers.add_parser('publications-per-gene')
+    parser_a.add_argument('--taxon-id', type=int)
+    parser_a.set_defaults(func=publications_per_gene)
+    args = parser.parse_args(sys.argv[1:])
+    return args.func(args)
 
 if __name__ == '__main__':
     sys.exit(main())
